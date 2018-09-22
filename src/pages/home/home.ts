b97f21b8d86929bf217657ca6689e3c30b994a17
xuanphuoc129 <20138686@student.hust.edu.vn@gmail.com>
2018-09-22 18:05:40 +0700

    push code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,6 +10,14 @@ export class HomePage {
   girl_url: string = "./assets/imgs/viettel_trasau_02.png";
   text_url: string = "./assets/imgs/viettel_trasau_03.png";
   phone_number: string = "096.2018.555";
+
+  items : Array<any> = [
+    {title: "siêu rẻ", icon: "viettel-call", subtitle: "Chỉ từ", textbold: "100đ/phút", description: "Combo thoại + data siêu tiết kiệm lên tới 20%" },
+    {title: "ưu đãi", icon: "viettel-home", subtitle: "Giao hàng tại nhà", textbold: "miễn phí", description: "Chọn SIM số đẹp 10 số" },
+    {title: "tiện lợi", icon: "viettel-money", subtitle: "Dùng trước thanh toán sau", textbold: "hòa mạng đơn giản", description: "- 096.2018.555 - <br> - Cửa hàng viettel - <br> - Ứng dụng My Viettel - <br> - www.shop.viettel.vn -" },
+    {title: "linh hoạt", icon: "viettel-payment", subtitle: "Dễ dàng", textbold: "thanh toán", description: "- Thẻ cào viettel - <br> - Qua BankPlus Viettel Pay - <br> - Internet banking - <br> - Tới cửa hàng viettel -" },
+  ];
+
   constructor(public navCtrl: NavController) {
 
   }
